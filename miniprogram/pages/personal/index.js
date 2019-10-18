0b4d7f0082a26749437304665ac3ee978fc39973
CcViFong <806763385@qq.com>
2019-10-18 13:33:32 +0800

    all

--- miniprogram/pages/personal/index.js
+++ miniprogram/pages/personal/index.js
@@ -105,6 +105,27 @@ Page({
     });
   },
 
+  tryCanvas() {
+    let context = wx.createCanvasContext('canvasTest')
+    console.log()
+    // context.setStrokeStyle("#00ff00")
+    context.setLineWidth(5)
+    context.rect(0, 0, 200, 200)
+    context.stroke()
+    context.setStrokeStyle("#ff0000")
+    context.setLineWidth(2)
+    context.moveTo(160, 100)
+    context.arc(100, 100, 60, 0, 2 * Math.PI, true)
+    context.moveTo(140, 100)
+    context.arc(100, 100, 40, 0, Math.PI, false)
+    context.moveTo(85, 80)
+    context.arc(80, 80, 5, 0, 2 * Math.PI, true)
+    context.moveTo(125, 80)
+    context.arc(120, 80, 5, 0, 2 * Math.PI, true)
+    context.stroke()
+    context.draw()
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -116,7 +137,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.tryCanvas();
   },
 
   /**
